22502c5e4e808b6f304c23233aa665cededa6dcd
Barbarall23 <159023182+Barbarall23@users.noreply.github.com>
2024-02-07 12:47:30 +0100

    Add files via upload

---
+++ practicagestiondatosr.R
@@ -0,0 +1,47 @@
+Sys.which("make")
+
+ejercicio 1
+data_frame <- data.frame(
+  yacimiento = c("Itálica", "Necrópolis de Carmona", "Carambolo"),
+  edad = c(1800, 1850, 1910)
+)
+print(data_frame)
+mean(1800, 1850, 1910)
+
+ejercicio 2
+data_frame <- data.frame(
+  yacimiento = c("Itálica", "Necrópolis de Carmona", "Caramboloa"),
+  cantidad_artefactos = c(5, 10, 4)
+)
+print(data_frame)
+valor_suma <- 5+10+4
+print(valor_suma)
+
+ejercicio 3
+data_frame <- data.frame(
+  yacimeinto = c("Itálica", "Necrópolis de Carmona", "Carambolo"),
+  profundidad_hallazgos = c(3, 5, 8)
+)
+print(data_frame)
+profundidad_máxima_hallazgos = c(9)
+
+ejercicio 4
+data_frame <- data.frame(
+  yacimiento = c("Itálica", "Necrópolis de Carmona", "Carambolo"),
+  tipos_de_materiales = c("cerámica", "hueso", "lítico")
+)
+print(data_frame)
+tipos_materiales_distintos = c(3)
+
+ejercicio 5
+data_frame <- data.frame(
+  yacimiento = c("Itálica", "Necrópolis de Carmona", "Carambolo"),
+  años_excavaciones = c(2000, 2005, 2010)
+)
+print(data_frame)
+años_de_excavaciones = (3)
+
+ejercicio 6
+matriz1 <- matrix(c(1,2,3,4,5,6), 
+                  nrow = 10, ncol = 3,
+                  byrow = T)
